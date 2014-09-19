0fe21f639f931b1b1e1c5441693d8f81783d2906
Alen Bou-Haidar <alencool@gmail.com>
2014-09-19 20:08:09 +1000

    Completed Lexer module

---
+++ Parser.pm
@@ -0,0 +1,7 @@
+    my @kw_block = qw(if, elif, else, for, while, def);
+    my @kw_line = qw(return, break, continue, print, in);            
+    my @kw_logical = qw(not and or);
+    my @kw_error = qw(del is raise assert from lambda global try
+                      class except import yield exec finally pass);    
+    my %kw_bool = (True => 1, False => 0);
+
