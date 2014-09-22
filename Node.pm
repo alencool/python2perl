9e50b95f02179abf238b1e093abad4108ba2978c
Alen Bou-Haidar <alencool@gmail.com>
2014-09-22 23:13:06 +1000

    Moved Node subclasses into one module

--- Node.pm
+++ Node.pm
@@ -1,14 +1,19 @@
-package Node;
-
 use strict;
 use warnings;
-use Carp;
+
+#-----------------------------------------------------------------------
+#  ___                 _  _         _     
+# | _ ) __ _ ___ ___  | \| |___  __| |___ 
+# | _ \/ _` (_-</ -_) | .` / _ \/ _` / -_)
+# |___/\__,_/__/\___| |_|\_\___/\__,_\___|
+# 
+#-----------------------------------------------------------------------
+package Node;
 
 # constructor
 sub new {
     my ($class, $value) = @_;
     my $self = { value      => '',      # str value to infer kind
-                 kind       => '',      # what kind of node is it
                  type       => undef,   # for use in infering var types
                  comment    => '',      # possible comment attached
                  complete   => 1,       # is contents full
@@ -26,13 +31,11 @@ sub new {
 sub _init {
     my ($self, $value) = @_;
     $self->{value} = $value;
-    $self->{kind} = 'NODE';
 }
 
 # get node kind
 sub kind {
-    my ($self) = @_;
-    return $self->{kind};
+    return 'NODE';
 }
 
 # get node value
@@ -41,16 +44,37 @@ sub value {
     return $self->{value};
 }
 
-# returns "kind|value"
+# returns signature as "kind|value"
 sub signature {
     my ($self) = @_;
-    return $self->{kind} . '|' . $self->{value};   
+    return $self->kind . '|' . $self->value;   
 }
 
 # add child node
 sub add_child {
     my ($self, $node) = @_;
-    carp __PACKAGE__ . " not accepting child nodes at this time"
+
+    if ($self->_on_event_add_child($node)){
+        # All systems are go, can append child node to last rootlist
+        my $rootlist = $self->{children};
+        my $childlist = @$rootlist[$#$rootlist];
+        push @$childlist, $node;
+        $node->_set_parent($self);
+        #TODO set siblings!
+    }
+}
+
+# on add child event, return true to add child, false to ignore
+sub _on_event_add_child {
+    my ($self, $node) = @_;
+    return 0;
+}
+
+# create new child list
+sub _new_child_list {
+    my ($self) = @_;
+    my $rootlist = $self->{children};
+    push @$rootlist, [];
 }
 
 # sets a nodes comment
@@ -79,12 +103,13 @@ sub is_leaf {
 
 # return true if node represents a statement
 sub is_statement {
-    return  0;
+    my ($self) = @_;
+    return ($self->is_simple || $self->is_compound);
 }
 
 # return true if node represents a compount statement
 sub is_compound {
-    return  0;
+    return 0;
 }
 
 # return true if node represents a simple statement
@@ -124,14 +149,328 @@ sub set_left_sibling {
     $self->{prev} = $prev;
 }
 
+#-----------------------------------------------------------------------
+#  ___ _                   _     _  _         _        
+# | __| |___ _ __  ___ _ _| |_  | \| |___  __| |___ ___
+# | _|| / -_) '  \/ -_) ' \  _| | .` / _ \/ _` / -_|_-<
+# |___|_\___|_|_|_\___|_||_\__| |_|\_\___/\__,_\___/__/ 
+#
+#-----------------------------------------------------------------------
 
 
+package Node::Arithmetic;
+use parent 'Node';
+
+sub kind {
+    return 'ARITHMETIC';
+}
+
+#-----------------------------------------------------------------------
+package Node::Bitwise;
+use parent 'Node';
+
+sub kind {
+    return 'BITWISE';
+}
+
+#-----------------------------------------------------------------------
+package Node::Comparison;
+use parent 'Node';
 
 
-# unshift
-# shift
-# push
-# pop
-# remove
+sub _init {
+    my ($self, $value) = @_;
+    $value = '!=' if ($value eq '<>');
+    $self->{value} = $value;
+}
+
+sub kind {
+    return 'COMPARISON';
+}
+
+#-----------------------------------------------------------------------
+package Node::Encloser;
+use parent 'Node';
+
+
+sub _init {
+    my ($self, $value) = @_;
+
+    if  ($value =~ m/^[])}]$/) {
+        # All set up
+    } else {
+        $self->{complete} = 0;
+        $self->{children} = [[]];
+        if ($value eq '[') {
+            $self->{closevalue} = ']';
+        } elsif ($value eq '{') {
+            $self->{closevalue} = '}';
+        } elsif ($value eq '(') {
+            $self->{closevalue} = ')';
+        } else {
+            die "Not a bracket.";
+        }
+
+    } else {
+        die "Not a bracket.";
+    }
+
+    $self->{value} = $value;
+    
+}
+
+sub kind {
+    return 'ENCLOSER';
+}
+
+sub add_child {
+    my ($self, $node) = @_;
+
+    if ($node->is_statement) {
+        croak __PACKAGE__ . " does not accept statments";
+    }
+
+    my $children = $self->{children};
+    if ($node->signature ~~ ['SEPERATOR|,', 'SEPERATOR|:']){
+        # append new list
+        push @$children [];
+
+    } elsif ($node->signature eq $self->{closevalue}) {
+        # statement complete
+        $self->{complete} = 1;
+
+    } else {
+        # push child onto last list
+        $exp = @$children[$#$children];
+        push @$exp $node;
+        $node->_set_parent($self);
+    }
+
+}
+
+#-----------------------------------------------------------------------
+package Node::Identifier;
+use parent 'Node';
+
+sub kind {
+    return 'IDENTIFIER';
+}
+
+#-----------------------------------------------------------------------
+package Node::In;
+use parent 'Node';
+
+sub kind {
+    return 'IN';
+}
+
+
+#-----------------------------------------------------------------------
+package Node::Logical;
+use parent 'Node';
+
+sub kind {
+    return 'LOGICAL';
+}
+
+#-----------------------------------------------------------------------
+package Node::Number;
+use parent 'Node';
+
+sub kind {
+    return 'NUMBER';
+}
+
+#-----------------------------------------------------------------------
+package Node::Seperator;
+use parent 'Node';
+
+sub kind {
+    return 'SEPERATOR';
+}
+
+#-----------------------------------------------------------------------
+package Node::String;
+use parent 'Node';
+
+sub kind {
+    return 'STRING';
+}
+
+sub is_raw {
+    my ($self) = @_;
+    my $char = substr $self->{value} 0 1;
+
+    return ($char eq "'");
+}
+
+#-----------------------------------------------------------------------
+#  ___ _            _       _  _         _        
+# / __(_)_ __  _ __| |___  | \| |___  __| |___ ___
+# \__ \ | '  \| '_ \ / -_) | .` / _ \/ _` / -_|_-<
+# |___/_|_|_|_| .__/_\___| |_|\_\___/\__,_\___/__/
+#             |_|                                 
+#-----------------------------------------------------------------------
+package Node::Simple;
+use parent 'Node';
+
+sub is_simple {
+    return  1;
+}
+
+sub kind {
+    return 'SIMPLE';
+}
+
+sub _init {
+    my ($self, $value) = @_;
+    $self->{value} = $value;
+    $self->{complete} = 0;
+}
+
+sub _on_event_add_child {
+    my ($self, $node) = @_;
+
+    if ($node->is_statement) {
+        croak __PACKAGE__ . " does not accept statments";
+    }
+
+    my $add_child = 1;
+    if ($node->signature eq 'SEPERATOR|,') {
+        $self->_new_child_list;
+
+    } elsif ($node->signature eq 'SEPERATOR|;') {
+        # statement complete
+        $self->{complete} = 1;
+    }
+    return $add_child;
+}
+
+#-----------------------------------------------------------------------
+package Node::Assignment;
+use parent 'Node::Simple';
+
+sub _init {
+    my ($self, $value) = @_;
+    $self->SUPER::_init($value);
+    $self->{complete} = 1;
+}
+
+sub kind {
+    return 'ASSIGNMENT';
+}
+#-----------------------------------------------------------------------
+package Node::Blank;
+use parent 'Node::Simple';
+
+
+sub _init {
+    my ($self, $value) = @_;
+    $self->SUPER::_init($value);
+    $self->{complete} = 1;
+}
+
+sub kind {
+    return 'BLANK';
+}
+
+#-----------------------------------------------------------------------
+package Node::Break;
+use parent 'Node::Simple';
+
+sub _init {
+    my ($self, $value) = @_;
+    $self->SUPER::_init($value);
+    $self->{complete} = 1;
+}
+
+sub kind {
+    return 'BREAK';
+}
+
+#-----------------------------------------------------------------------
+package Node::Continue;
+use parent 'Node::Simple';
+
+sub _init {
+    my ($self, $value) = @_;
+    $self->SUPER::_init($value);
+    $self->{complete} = 1;
+}
+
+sub kind {
+    return 'CONTINUE';
+}
+
+#-----------------------------------------------------------------------
+package Node::Expression;
+use parent 'Node::Simple';
+
+sub kind {
+    return 'EXPRESSION';
+}
+
+
+#-----------------------------------------------------------------------
+package Node::Print;
+use parent 'Node::Simple';
+
+sub kind {
+    return 'PRINT';
+}
+
+#-----------------------------------------------------------------------
+package Node::Return;
+use parent 'Node::Simple';
+
+sub kind {
+    return 'RETURN';
+}
+
+
+#-----------------------------------------------------------------------
+#   ___                                  _   _  _         _        
+#  / __|___ _ __  _ __  ___ _  _ _ _  __| | | \| |___  __| |___ ___
+# | (__/ _ \ '  \| '_ \/ _ \ || | ' \/ _` | | .` / _ \/ _` / -_|_-<
+#  \___\___/_|_|_| .__/\___/\_,_|_||_\__,_| |_|\_\___/\__,_\___/__/
+#                |_|                                               
+#-----------------------------------------------------------------------
+package Node::Compound;
+use parent 'Node';
+
+sub is_compound {
+    return  1;
+}
+
+sub kind {
+    return 'COMPOUND';
+}
+
+#-----------------------------------------------------------------------
+package Node::Code;
+use parent 'Node';
+
+
+# initialization
+sub _init {
+    my ($self) = @_;
+    $self->{complete} = 0;
+}
+
+sub kind {
+    return 'CODE';
+}
+
+sub add_child {
+    my ($self, $node) = @_;
+
+    if (not $node->is_statement) {
+        croak __PACKAGE__ . " only accepts statments";
+    }
+    
+    push @{$self->{children}} $node;
+    $node->_set_parent($self);
+
+}
 
 1;
