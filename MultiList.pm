0387b90b3fcccbaa7d75d35899cacd428ef91398
Alen Bou-Haidar <alencool@gmail.com>
2014-09-24 08:27:56 +1000

    Added methods to MultiList class

--- MultiList.pm
+++ MultiList.pm
@@ -2,6 +2,14 @@
 #  MultiList.pm
 #  Defines a MultiList class, for the simple creation and manipulation
 #  of arrays of lists.
+#       
+#               peg0    peg1    peg2        pegN-1   
+#           -----#-------#-------#--- ... ---#-----------
+#                1       1       1           1
+#                2       2       2           2
+#                        3       3           3
+#                        4                   4
+#                        5
 #
 #  Created by Alen Bou-Haidar on 24/09/14, edited 24/9/14
 #
@@ -10,5 +18,58 @@ package MultiList;
 
 use strict;
 use warnings;
+
+# constructor
+sub new {
+    my ($class, @args) = @_;
+    my $self = { pegs => [[]] };
+    my $object = bless $self, $class;
+    return $object;
+}
+
+# returns number of pegs
+sub get_peg_count {
+    my ($self) = @_;
+    return scalar @{$self->{pegs};
+}
+
+# append item to peg at peg_index
+sub append_to_peg {
+    my ($self, $peg_index, $item) = @_;
+    $peg = $self->get_peg($peg_index);
+    push @$peg, $item;
+}
+
+# append item to last peg
+sub append_to_lastpeg {
+    my ($self, $item) = @_;
+    my pegs = _pegs
+    my $lastpeg_idex = $self->get_peg_count - 1;
+    $self->append_to_peg($lastpeg_idex, $item);
+}
+
+# creates new peg at end of pegs array
+sub new_peg {
+    my ($self) = @_;
+    push @{$self->{pegs}}, [];
+}
+
+# get peg reference at peg_index
+sub get_peg {
+    my ($self, $peg_index) = @_;
+    return $self->_pegs[$peg_index];
+}
+
+# returns true if single peg with no items
+sub is_empty {
+    my ($self) = @_;
+    return ($self->get_peg_count == 1 and @{$self->get_peg(0)} == 0);
+}
+
+# returns pegs as a list
+sub _pegs {
+    my ($self) = @_;
+    return (@{$self->{pegs}});
+}
 
 1;
