4789f6f34eb8c59a9fe966fd2da4a393b3b6546c
Alen Bou-Haidar <alencool@gmail.com>
2014-09-23 16:23:19 +1000

    Refactored Lexer

--- Lexer.pm
+++ Lexer.pm
@@ -2,251 +2,212 @@ package Lexer;
 
 use strict;
 use warnings;
-use Words;
+use Node;
+use feature 'switch';
+use constant KW_ERROR  => qw(del is raise assert from lambda global 
+                             try class except yield exec finally pass);
 
-########################################################################
-#                         Public Methods                               #
-########################################################################
+
+# Compile Regex for each token type
+my $reIndent        = qr/(^\s*)[^#]/;
+my $reFloat         = qr/^[+-]?[0-9]*[.][0-9]*(e[-+]?[0-9]+)?/i;
+my $reHex           = qr/^[+-]?0x[a-f0-9]+/i;
+my $reOctal         = qr/^[+-]?0[0-7]*/;
+my $reDecimal       = qr/^[+-]?[1-9][0-9]*/;
+my $reAssignment    = qr/^(<<|>>|\*\*|\/|[-+*%&|^])?=/;
+my $reArithmetic    = qr/^(\+|-|\*\*|\*|\/|%)/;
+my $reBitwise       = qr/^(<<|>>|&|\||\^|~)/;
+my $reComparison    = qr/^(<=|>=|==|!=|<>|<|>)/;
+my $reString        = qr/^(r?)("|')(?!\2\2)(.*?)(?<!\\)\2/i;
+my $reEncloser      = qr/^[][(){}]/;
+my $reSeperator     = qr/^[.,:;]/;
+my $reComment       = qr/\s*#/;
+my $reWhitespace    = qr/^\s*/;
+my $reWord          = qr/^[a-z_][a-z0-9_]*/i;
 
 # constructor
 sub new {
     my ($class, @args) = @_;
-    my $self = { tokens => [] };
+    my $self = { nodes => [] };
     my $object = bless $self, $class;
     return $object;
 }
 
-# returns true if more tokens
+# returns true if more nodes
 sub has_next {
     my $self = shift;
-    return @{$self->{tokens}} > 0;
+    return @{$self->{nodes}} > 0;
 }
 
 # returns next token
 sub next {
     my $self = shift;
-    return shift @{$self->{tokens}}; 
+    return shift @{$self->{nodes}}; 
 }
 
-# converts a list of lines into tokens
+# converts a list of lines into node tokens
 sub tokenize {
     my ($self, @lines)  = @_;
-    my @tokens = ();
+    my @nodes = ();
+
     for my $line (@lines) {
         chomp $line;
         my $str = $line;            # str will be consumed
-        my @token_buffer = ();      # hold tokens incase of error
+        my @token_buffer = ();      # hold nodes incase of error
         my $comment = '';           # holds the comment if one exists
-        my $token;
+        my $node;
 
         # scan at start for indent
-        if ($str =~ m/(^\s*)[^#]/) {
-            ($str, $token) = $self->_scan_indent($str);
-            push @token_buffer, $token;
+        if ($str =~ /$reIndent/) {
+            $node = $self->_get_indent($str);
+            push @token_buffer, $node;
         }
-        
-        # scan for appropriate token based on 'peek' re
-        # and consume the part of $str relating to the token.
+
+        # consume $str and create node nodes.
         while($str) {
-            if ($str =~ m/^[][(){}]/) {
-                ($str, $token) = $self->_scan_encloser($str);
 
-            } elsif ($str =~ m/^[+-]?[.]?[0-9]/) {
-                ($str, $token) = $self->_scan_number($str);
-            
-            } elsif ($str =~ m/^[.,:;]/) {
-                ($str, $token) = $self->_scan_seperator($str);
+            $node = $self->_extract_node(\$str);
 
-            } elsif ($str =~ m'^[-+*/%=&|^><~!]') {
-                ($str, $token) = $self->_scan_symbol($str);
-            
-            } elsif ($str =~ m/^u?r?('|")/i) {
-                ($str, $token) = $self->_scan_string($str);
-
-            } elsif ($str =~ m/^[a-zA-Z_]/) {
-                ($str, $token) = $self->_scan_word($str);
-            
-            } elsif ($str =~ m'^\s*#') {
-                # rest of str is a comment
-                $comment = $str;
-                last;
-
-            } elsif ($str =~ m'^\s+') {
-                # ignore whitespace
-                $str = substr $str, length($&);
-                next;
-
-            } else {
-                $token = {type => 'error'};
-            }
-            
-            if ($$token{type} eq 'error') {
+            if ($node->kind eq 'ERROR') {
                 # return entire line as a comment
                 $comment = "#$line";
                 @token_buffer = ();
                 last;
+            } elsif ($node->kind eq 'WHITESPACE') {
+                # ignore whitespace
+                next;
+            } elsif ($node->kind eq 'COMMENT') {
+                $comment = $node->value;
+                last;
             } else {
-                push @token_buffer, $token;
+                push @token_buffer, $node;
             }   
         }
 
-        # push entire line and buffer onto tokens list
-        push @tokens, {type => 'start', value => $line};
-        push @tokens, @token_buffer;
-        push @tokens, {type => 'end', value => $comment};
+        # push entire line and buffer onto nodes list
+        push @nodes, new Node::Start($line);
+        push @nodes, @token_buffer;
+        push @nodes, new Node::End($comment);
     }
 
-    $self->{tokens} = \@tokens;
+    $self->{nodes} = \@nodes;
 }
 
-
-########################################################################
-#                         Private Methods                              #
-########################################################################
-
-
-# scan for indent
-sub _scan_indent {
+# Extracts a token node from $str reference
+sub _extract_node {
     my ($self, $str) = @_;
-    my $token;
-    $str =~ m/(^\s*)[^#]/;
+    my ($node, $value);
+
+
+    given ($$str) {
+        when (/$reEncloser/)    { $node = new Node::Encloser($&);
+                                  $$str =~ s/$reEncloser// }
+        
+        when (/$reFloat/)       { $node = new Node::Number($&);
+                                  $$str =~ s/$reFloat// }
+        
+        when (/$reHex/)         { $node = new Node::Number($&);
+                                  $$str =~ s/$reHex// }
+        
+        when (/$reOctal/)       { $node = new Node::Number($&);
+                                  $$str =~ s/$reOctal// }
+        
+        when (/$reDecimal/)     { $node = new Node::Number($&);
+                                  $$str =~ s/$reDecimal// }
+        
+        when (/$reSeperator/)   { $node = new Node::Number($&);
+                                  $$str =~ s/$reSeperator// }
+        
+        when (/$reAssignment/)  { $node = new Node::Assignment($&);
+                                  $$str =~ s/$reAssignment// }
+        
+        when (/$reArithmetic/)  { $node = new Node::Arithmetic($&);
+                                  $$str =~ s/$reArithmetic// }
+        
+        when (/$reBitwise/)     { $node = new Node::Bitwise($&);
+                                  $$str =~ s/$reBitwise// }
+        
+        when (/$reComparison/)  { $node = new Node::Comparison($&);
+                                  $$str =~ s/$reComparison// }
+        
+        when (/$reString/)      { $value = $3;
+                                  if (lc($1) eq 'r') {
+                                       # raw string
+                                       $value = qq('$value');
+                                  } else {
+                                       #escape $ symbols
+                                       $value =~ s/\$/\\\$/g;
+                                       #escape @ symbols
+                                       $value =~ s/@/\\@/g;   
+                                       $value = qq("$value");
+                                  }
+                                  $node = new Node::String($value);
+                                  $$str =~ s/$reString// }
+        
+        when (/$reWord/)        { $node = $self->_get_word_node($&);
+                                  $$str =~ s/$reWord// }
+
+        when (/$reComment/)     { $node = new Node::Comment($$str) }
+
+        when (/$reWhitespace/)  { $node = new Node::Whitespace();
+                                  $$str =~ s/$reWhitespace// }
+
+        default                 { $node = new Node::Error() }
+    }
+
+    return $node;
+}
+
+# Create correct node based on word
+sub _get_word_node {
+    my ($self, $word) = @_;
+    my $node;
+    given ($word) {
+        when ('if')         { $node = new Node::If() }
+        when ('elif')       { $node = new Node::Elif() }
+        when ('else')       { $node = new Node::Else() }
+        when ('for')        { $node = new Node::For() }
+        when ('while')      { $node = new Node::While() }
+        when ('def')        { $node = new Node::Def() }
+        when ('return')     { $node = new Node::Return() }
+        when ('break')      { $node = new Node::Break() }
+        when ('continue')   { $node = new Node::Continue() }
+        when ('print')      { $node = new Node::Print() }
+        when ('not')        { $node = new Node::Logical('not') }
+        when ('and')        { $node = new Node::Logical('and') }
+        when ('or')         { $node = new Node::Logical('or') }
+        when ('True')       { $node = new Node::Number('1') }
+        when ('False')      { $node = new Node::Number('0') }
+        when ('in')         { $node = new Node::In() }
+        when ('import')     { $node = new Node::Invisible() }
+        when ([KW_ERROR])   { $node = new Node::Error() }
+        default             { $node = new Node::Identifier($word) }
+    }
+
+    return $node;
+}
+
+# extract unix compatible expansion of tabs
+sub _get_indent {
+    my ($self, $str) = @_;
+    my $node;
     # line has statements, find indent 
     # TAB 1-8 spaces to get multiple of 8 upto and including TAB
+
     my $whitespace = $1;
-    my $indent = 0;
+    my $num_spaces = 0;
     while ($whitespace =~/( +|\t)/g) {
           if ($1 eq "\t") {
-                $indent += 8 - ($indent % 8);
+                $num_spaces += 8 - ($num_spaces % 8);
           } else {
-                $indent += length($1);
+                $num_spaces += length($1);
           }
     }
+
     #remove front indent
-    $str =~ s/^\s*//;
-    $token = {type => 'indent', value => $indent};
+    $node = new Node::Indent($num_spaces);
     
-    return ($str, $token);
-}
-
-sub _scan_number {
-    my ($self, $str) = @_;
-    my $token;
-    
-    if ($str =~ m/^[+-]?[0-9]*[.][0-9]*(e[-+]?[0-9]+)?/i || # float
-        $str =~ m/^[+-]?0x[a-f0-9]+/i ||                    # hex
-        $str =~ m/^[+-]?0[0-7]*/      ||                    # octal
-        $str =~ m/^[+-]?[1-9][0-9]*/ ) {                    # decimal
-        $str = substr $str, length($&);
-        $token = {type => 'number', value => lc($&)};
-    } else {
-        $token = {type => 'error'};
-    }
-
-    return ($str, $token);
-}
-
-sub _scan_symbol {
-    # assignment +=    -=      *=      /=     %=      =
-    #            &=    |=      ^=      >>=    <<=     **=
-    # arithmatic +     -       *       **     /       %
-    # bitwise    <<    >>      &       |      ^       ~
-    # comparison <     >       <=      >=     ==      !=    <>
-    my ($self, $str) = @_;
-    my $token;
-    if ($str =~ m/^(<<|>>|\*\*|\/|[-+*%&|^])?=/) {
-        $token = {type => 'assignment', value => $&};
-    } elsif ($str =~ m/^(\+|-|\*\*|\*|\/|%)/) {
-        $token = {type => 'arithmatic', value => $&};
-    } elsif ($str =~ m/^(<<|>>|&|\||\^|~)/) {
-        $token = {type => 'bitwise', value => $&};
-    } elsif ($str =~ m/^(<=|>=|==|!=|<>|<|>)/) {
-        $& =~ s/<>/!=/;
-        $token = {type => 'comparison', value => $&};
-    }
-    $str = substr $str, length($&);
-
-    return ($str, $token);
-}
-
-sub _scan_word {
-    my ($self, $str) = @_;
-    my ($token, $type, $word);
-    $str =~ m/^[a-z_][a-z0-9_]*/i;
-    $word = $&;
-    if ($word ~~ [KW_COMPOUND]) {
-        $type = 'kwcompound';
-    } elsif ($word ~~ [KW_SIMPLE]) {
-        $type = 'kwsimple';
-    } elsif ($word ~~ [KW_LOGIC]) {
-        $type = 'kwlogic';
-    } elsif ($word ~~ [KW_BOOL]) {
-        $type = 'kwbool';
-    } elsif ($word ~~ [KW_ERROR]) {
-        $type = 'error';
-    } else {
-        $type = 'identifier';
-    }
-    $token = {type => $type, value => $word};
-    $str = substr $str, length($word);
-
-    return ($str, $token);
-}
-
-sub _scan_encloser {
-    my ($self, $str) = @_;
-    my %enclosers = ('(' => 'roundopen',
-                     ')' => 'roundclosed',
-                     '{' => 'curlyopen',
-                     '}' => 'curlyclosed',
-                     '[' => 'squareopen',
-                     ']' => 'squareclosed');
-    my $char = substr $str, 0, 1;
-    my $token = {type => $enclosers{$char}, value => $char};
-    $str = substr $str, 1;
-
-    return ($str, $token);
-}
-
-sub _scan_seperator {
-    my ($self, $str) = @_;
-    my %seperators = ('.' => 'dot',
-                      ',' => 'comma',
-                      ':' => 'collon',
-                      ';' => 'semicolon');
-    my $char = substr $str, 0, 1;
-    my $token = {type => $seperators{$char}, value => $char};
-    $str = substr $str, 1;
-
-    return ($str, $token);
-}
-
-sub _scan_string {
-    my ($self, $str) = @_;
-    my $token;
-    if ($str =~ m/^u/i || $str =~ m/^u?r?("""|''')/i) {
-        # for simplicity unicode and multiline strings are not supported
-        $token = {type => 'error'};
-    } elsif ($str =~ m/^(r?)"((\\.|.)*?)"/i ||
-             $str =~ m/^(r?)'((\\.|.)*?)'/i) {
-        # complete string
-        $str = substr $str, length($&);
-        my $string_value = $2;
-        if (lc($1) eq 'r') {
-            # raw string
-            $string_value = qq('$string_value');
-        } else {
-            # regular string
-            $string_value =~ s/\$/\\\$/g;
-            $string_value =~ s/@/\\@/g;
-            $string_value = qq("$string_value");
-        }
-        $token = {type => 'string', value => $string_value};
-    } else {
-        # incomplete string 
-        $token = {type => 'error'};
-    }
-
-    return ($str, $token);
+    return $node;
 }
 
 1;
